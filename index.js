76a61c3cb0680b5549bd731595f8810d3ec29686
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2017-11-29 00:20:01 +1100

    Fixed incorrect process env variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const nodeEnv = process.env.NODE_ENV || 'development'
-const port = process.env.POST || 8000
+const port = process.env.PORT || 8000
 const isDevelopment = nodeEnv === 'development'
 
 const app = new express()
