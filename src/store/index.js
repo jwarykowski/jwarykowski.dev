38b88d7aa795bec3e498dd9ad7258b13d2554eb1
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2017-11-28 23:47:51 +1100

    Refactored exports to prevent redux-logger become non dev dependancy

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
-const development = require('./development')
-const production = require('./production')
-
 const isProduction = process.env.NODE_ENV === 'production'
-const store = isProduction ? production : development
 
-module.exports = store
+if (isProduction) {
+  module.exports = require('./production')
+} else {
+  module.exports = require('./development')
+}
