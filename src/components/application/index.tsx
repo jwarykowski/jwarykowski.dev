c41b145bfeabf9c677e9e7099fa70310a11a72a1
Jonathan Warykowski <jonathan.warykowski@gmail.com>
2020-03-28 12:59:08 +1100

    patch updated information and links

--- src/components/application/index.tsx
+++ src/components/application/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Hero, Image, Link, SubTitle, Tabs, Title } from 'reactbulma'
+import { Button, Container, Hero, Image, Link, SubTitle, Tabs, Title } from 'reactbulma'
 import React from 'react'
 
 export default function Application() {
@@ -6,12 +6,12 @@ export default function Application() {
     <Hero fullheight medium primary>
       <Hero.Body>
         <Container>
-          <Title>jonathan warykowski</Title>
+          <Title is="1" spaced>jonathan warykowski</Title>
           <SubTitle>
-            A British Javascript Developer{' '}
-            <Link href="https://lighthouse.io">@lighthouse.io</Link> in
-            Melbourne, Australia.
+            <p>A British software developer <Link href="https://lighthouse.io">@lighthouse.io</Link> in Melbourne, Australia.</p>
+            <p>Loves <Link href="https://en.wikipedia.org/wiki/JavaScript">#JS</Link>, <Link href="https://www.richmondfc.com.au">#Tigers</Link>, <Link href="https://www.nufc.co.uk">#NUFC</Link> and the occasional <Link href="https://en.wikipedia.org/wiki/Fig_roll">#FigRoll</Link>.</p>
           </SubTitle>
+          <Button class="button is-primary is-inverted"><Link href="mailto:name@rapidtables.com">Contact Me</Link></Button>
         </Container>
       </Hero.Body>
       <Hero.Foot>
