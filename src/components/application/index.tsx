7336753b192271492850bf46f51885f941d60c61
Jonathan Warykowski <jonathan.warykowski@gmail.com>
2020-06-14 12:27:38 +1000

    patch added .dotfiles link

--- src/components/application/index.tsx
+++ src/components/application/index.tsx
@@ -1,4 +1,13 @@
-import { Button, Container, Hero, Image, Link, SubTitle, Tabs, Title } from 'reactbulma'
+import {
+  Button,
+  Container,
+  Hero,
+  Image,
+  Link,
+  SubTitle,
+  Tabs,
+  Title,
+} from 'reactbulma'
 import React from 'react'
 
 export default function Application() {
@@ -6,18 +15,46 @@ export default function Application() {
     <Hero fullheight medium primary>
       <Hero.Body>
         <Container>
-          <Title is="1" spaced>jonathan warykowski</Title>
+          <Title is="1" spaced>
+            jonathan warykowski
+          </Title>
           <SubTitle>
-            <p>A British software developer <Link href="https://lighthouse.io">@lighthouse.io</Link> in Melbourne, Australia.</p>
-            <p>Loves <Link href="https://en.wikipedia.org/wiki/JavaScript">#JS</Link>, <Link href="https://www.richmondfc.com.au">#Tigers</Link>, <Link href="https://www.nufc.co.uk">#NUFC</Link> and the occasional <Link href="https://en.wikipedia.org/wiki/Fig_roll">#FigRoll</Link>.</p>
+            <p>
+              A British software developer{' '}
+              <Link href="https://lighthouse.io">@lighthouse.io</Link> in
+              Melbourne, Australia.
+            </p>
+            <p>
+              Loves{' '}
+              <Link href="https://en.wikipedia.org/wiki/JavaScript">#JS</Link>,{' '}
+              <Link href="https://www.richmondfc.com.au">#Tigers</Link>,{' '}
+              <Link href="https://www.nufc.co.uk">#NUFC</Link> and the
+              occasional{' '}
+              <Link href="https://en.wikipedia.org/wiki/Fig_roll">
+                #FigRoll
+              </Link>
+              .
+            </p>
           </SubTitle>
-          <Button class="button is-primary is-inverted"><Link href="mailto:name@rapidtables.com">Contact Me</Link></Button>
+          <Button class="button is-primary is-inverted">
+            <Link href="mailto:name@rapidtables.com">Contact Me</Link>
+          </Button>
         </Container>
       </Hero.Body>
       <Hero.Foot>
         <Container>
           <Tabs>
             <ul>
+              <li>
+                <strong>
+                  <a
+                    href="https://github.com/jwarykowski/.dotfiles"
+                    target="_blank"
+                  >
+                    .dotfiles
+                  </a>
+                </strong>
+              </li>
               <li>
                 <strong>
                   <a href="https://github.com/jwarykowski" target="_blank">
